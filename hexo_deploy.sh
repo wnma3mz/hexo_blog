51701a6708660fb8636f91220c0738fcda79014d
wnma3mz <wnma3mz@gmail.com>
2024-07-24 22:30:58 +0800

    Site updated: 2024-07-24 22:30:54

---
+++ hexo_deploy.sh
@@ -0,0 +1,6 @@
+hexo clean && hexo generate && hexo deploy --message "new article"
+hexo clean ;; hexo generate ;; hexo deploy --message "new article"
+# hexo clean && hexo g && hexo d
+# 本地预览
+hexo clean && hexo g && hexo s
+
