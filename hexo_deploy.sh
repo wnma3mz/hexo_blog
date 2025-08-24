bb45bf9dd08fad347a61e6e4d97499e329fde990
wnma3mz <wnma3mz@gmail.com>
2025-08-24 16:41:09 +0800

    VLM and MoE TopP

--- hexo_deploy.sh
+++ hexo_deploy.sh
@@ -1,5 +1,5 @@
 hexo clean && hexo generate && hexo deploy --message "new article"
-hexo clean ;; hexo generate ;; hexo deploy --message "new article"
+# hexo clean ;; hexo generate ;; hexo deploy --message "new article"
 # hexo clean && hexo g && hexo d
 # 本地预览
 hexo clean && hexo g && hexo s
